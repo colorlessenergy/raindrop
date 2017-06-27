3b15207c6a4614b3d2d9fb1f4a8230ba3975336b
colorlessenergy <brianmunozdev@gmail.com>
2017-06-27 15:19:56 -0700

    rain drop falls, canvas needs clearing

---
+++ main.js
@@ -0,0 +1,56 @@
+ // purple rain
+ // 1. create canvas height/width
+ // 2. create rain drops
+ //  a. random X and Y should be at before the actually seen top.
+ //  b. track Y and keep going down until reaches the max Y (bottom)
+ //  c. once reach bottom remove it than add another once
+
+const drop = [];
+var c = document.createElement('canvas');
+var canvas = c.getContext('2d');
+c.height = 500;
+c.width = 500;
+document.querySelector('body').appendChild(c);
+
+
+ class RainDrop {
+   constructor(x, y, w, h, ctx) {
+     this.x = x;
+     this.y = y;
+     this.w = w;
+     this.h = h;
+     this.ctx = ctx;
+
+   }
+
+   createRain() {
+     console.log('called');
+     this.ctx.fillStyle = "purple";
+     this.ctx.fill();
+     this.ctx.rect(this.x, this.y, this.w, this.h);
+   }
+
+   fall() {
+     this.y = this.y + 100;
+   }
+ }
+
+
+function update () {
+  canvas.clearRect(0, 0, c.height, c.width);
+  for (var i = 0; i < drop.length; i++) {
+    drop[i].createRain();
+    drop[i].fall();
+  }
+}
+
+function setUp () {
+  canvas.fillStyle = "black";
+  canvas.fillRect(0 ,0, c.width, c.height);
+  for (var i = 0; i < 5; i++) {
+    drop[i] = new RainDrop(Math.floor(Math.random() * 499), 100, 5, 15, canvas);
+  }
+  setInterval(update, 100)
+}
+
+setUp();
